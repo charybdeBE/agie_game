1aaa62dbdcbff0dfb0a8cc17c26e9e70ad9bc7dd
sylvain-dazy <sylvain.dazy@student.ulg.ac.be>
2016-03-31 13:32:49 +0200

    Changing name of files concerning the management activity + Layout updated (name of lists).

---
+++ app/src/main/java/be/ac/ulg/montefiore/group03/agilegame/Manager.java
@@ -0,0 +1,55 @@
+package be.ac.ulg.montefiore.group03.agilegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import be.ac.ulg.montefiore.group03.agilegame.Calendar;
+import be.ac.ulg.montefiore.group03.agilegame.gamelogic.GameLogic;
+
+public class Manager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+
+        Button toCal = (Button) findViewById(R.id.calendar_button);
+        toCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Calendar.class);
+
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
