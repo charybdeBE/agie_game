5bf3030c1457cddeb0d56ba112f793c1b6d0bc9b
sylvain-dazy <sylvain.dazy@student.ulg.ac.be>
2016-05-02 16:23:37 +0200

    Add more TODO

--- app/src/main/java/be/ac/ulg/montefiore/group03/agilegame/gamelogic/GameLogic.java
+++ app/src/main/java/be/ac/ulg/montefiore/group03/agilegame/gamelogic/GameLogic.java
@@ -12,6 +12,10 @@ import be.ac.ulg.montefiore.group03.agilegame.gamelogic.Events.Programmer_Event;
 import be.ac.ulg.montefiore.group03.agilegame.gamelogic.Journal.Journal;
 
 // TODO: interrest in and skills are sometimes doubled in the dialog box, why ?
+// TODO: Instanciate programmers with names
+// TODO: quand on recrute un nouveau membre, il doit etre retire du pole emploi immediatement
+// TODO: (pour l'instant il n'est retirer que lorsqu'on quitte l'activite et qu'on y revient apres)
+// TODO: design pattern is complete but still in the list of tasks ?!
 
 /**
  * Created by charybde on 08.03.16.
